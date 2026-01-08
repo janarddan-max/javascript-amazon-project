318430585a6da2eaed8504a78bcff3e5caa5f8f6
JanarddanSaharia <never.better.before12345@gmail.com>
2026-01-08 10:18:47 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,10 +4,53 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
 import { loadProducts } from '../data/products.js';
+import {loadCart} from '../data/cart.js'
 
+
+ Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve();
+        });
+    }),
+
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    })
+
+ ]).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+ });
+
+
+/*
+ new Promise((resolve)=>{
+    console.log('hii..');
+    loadProducts(()=>{
+        resolve();
+    });
+
+ }).then(()=>{
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();           
+        });
+    });
+
+ }).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+ })
+
+*/
+
+ /*
 loadProducts(()=>{
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 })
-
+*/
